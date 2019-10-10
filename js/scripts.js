e5ebb54dd6a889e2b07182bac8099d6a4bfd33b8
Zina <freeforcer@gmail.com>
2019-10-11 00:48:03 +0300

    fgdgd

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+};
+
+var lang = getUrlParameter('lang');
+if (lang != undefined) {
+	dropdownLang.innerHTML = lang.toUpperCase();
+	dropdownLang2.innerHTML = lang.toUpperCase();
+}
